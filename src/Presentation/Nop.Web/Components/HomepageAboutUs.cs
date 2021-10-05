5d2e236cdc5e104efcd958b806c157e93b80f3fc
romeohuy <romeohuy@gmail.com>
2021-10-06 00:01:22 +0700

    wip

---
+++ src/Presentation/Nop.Web/Components/HomepageAboutUs.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Nop.Services.Topics;
+using Nop.Web.Factories;
+using Nop.Web.Framework.Components;
+using Nop.Web.Models.Topics;
+
+namespace Nop.Web.Components
+{
+    public class HomepageAboutUs : NopViewComponent
+    {
+        private readonly ITopicModelFactory _topicModelFactory;
+
+        public HomepageAboutUs(ITopicModelFactory topicModelFactory)
+        {
+            _topicModelFactory = topicModelFactory;
+        }
+
+        /// <returns>A task that represents the asynchronous operation</returns>
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var homeAboutTopic = await _topicModelFactory.PrepareTopicModelBySystemNameAsync("HomeAbout");
+            var homeTestimonialsTopic = await _topicModelFactory.PrepareTopicModelBySystemNameAsync("HomeTestimonials");
+            return View(new HomeAboutModel{AboutModel = homeAboutTopic,HomeTestimonialModel = homeTestimonialsTopic});
+        }
+    }
+}
---
+++ src/Presentation/Nop.Web/Models/Home/HomeAboutModel.cs
@@ -0,0 +1,7 @@
+﻿using Nop.Web.Models.Topics;
+
+public class HomeAboutModel
+{
+    public TopicModel AboutModel { get; set; }
+    public TopicModel HomeTestimonialModel { get; set; }
+}
